b1add0ee247558b840d326692bcbfaa6fbd3562f
Katllyn Rezende <Katllynrezende@gmail.com>
2026-09-16 22:18:18 -0300

    Demonstração da instância única de AcessoDados

--- src/siga/Main.java
+++ src/siga/Main.java
@@ -3,25 +3,36 @@ package siga;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("=== SIGA - Atividade de Padrões Criacionais (Abstract Factory + Builder) ===\n");
+        System.out.println(
+                "=== SIGA - Atividade de Padrões Criacionais ===\n"
+        );
 
-        AcessoDados acesso = new AcessoDados();
-        acesso.conectar(new FabricaMySQL());
+        AcessoDados acesso = AcessoDados.getInstancia();
+        AcessoDados outroAcesso = AcessoDados.getInstancia();
+
+        System.out.println(
+                "Mesma instância: " + (acesso == outroAcesso)
+        );
+
         System.out.println();
+
+        acesso.conectar(new FabricaMySQL());
+
+        System.out.println();
+
         acesso.conectar(new FabricaPostgreSQL());
 
         System.out.println();
-        
-       String consulta = new ConsultaBuilder("aluno")
-        .comFiltro("curso = 'DSM'")
-        .comOrdenacao("nome")
-        .comLimite(50)
-        .comOffset(0)
-        .comTimeoutSegundos(30)
-        .somenteAtivos(true)
-        .construir();
 
-System.out.println("Consulta montada: " + consulta);
+        String consulta = new ConsultaBuilder("aluno")
+                .comFiltro("curso = 'DSM'")
+                .comOrdenacao("nome")
+                .comLimite(50)
+                .comOffset(0)
+                .comTimeoutSegundos(30)
+                .somenteAtivos(true)
+                .construir();
 
+        System.out.println("Consulta montada: " + consulta);
     }
 }
