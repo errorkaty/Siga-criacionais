45dda387f92d89bb1dd12023d1eaf15d08d3c500
Katllyn Rezende <Katllynrezende@gmail.com>
2026-09-16 22:17:54 -0300

    Transformação de AcessoDados em Singleton

--- src/siga/AcessoDados.java
+++ src/siga/AcessoDados.java
@@ -2,10 +2,19 @@ package siga;
 
 public class AcessoDados {
 
-public void conectar(FabricaBanco fabrica) {
+    private static final AcessoDados INSTANCIA = new AcessoDados();
+
+    private AcessoDados() {
+    }
+
+    public static AcessoDados getInstancia() {
+        return INSTANCIA;
+    }
+
+    public void conectar(FabricaBanco fabrica) {
         Conexao conexao = fabrica.criarConexao();
         Comando comando = fabrica.criarComando();
-        
+
         conexao.abrir();
         comando.executar("SELECT * FROM aluno");
     }
